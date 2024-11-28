d90a81113d62bf0f9a45f3fca6dbeafb37d21dbc
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-29 01:01:26 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,10 @@
+//const price = 45;
+// function sturcture
+function brushteeth(){
+
+    console.log('do it properly');
+    console.log('do it regularly');
+    console.log('for two time in a day');
+}
+//call function
+brushteeth();
---
+++ parameter.js
@@ -0,0 +1,17 @@
+function square(number){
+    console.log('value of the parameter:',number);
+    const square = number * number ;
+    console.log('square of the giben number is:',square);
+
+}
+square(4);
+square(6);
+square(9);
+square(33);
+
+
+function addition(num1, num2){
+    const sum = num1+num2;
+    console.log('additon of 2 numbers:',sum);
+}
+addition(6,5);
