30b7f7e838671d1c7f262961e1de3acb0165a215
ruhy567 <galiba.chowdhury56@gmail.com>
2024-12-01 01:06:47 +0600

    calculating

--- calculator.js
+++ calculator.js
@@ -9,18 +9,21 @@ function sum(num1, calculate, num2) {
     else if( calculate === '*'){
         result = num1 * num2;
     }
-    else if( calculate === '%'){
-        result = num1 % num2;
+    else if( calculate === '/'){
+        result = num1 / num2;
     }
 
     else {
         console.log('Unsupported operation');
         return;
     }
-    console.log('result:', result);
+    console.log(num1,calculate,num2,'the result:', result);
+    
+    
+
 }
 
 sum(6, '+', 5);
 sum(5, '-', 2);
 sum(3, '*', 2);
-sum(10, '%', 2);
+sum(10, '/', 5);
